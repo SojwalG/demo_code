905fd2f11df2e2ad18fe56a9c038389aa7ba9660
Sojwal <sojjwal17gulve@gmail.com>
2023-01-29 23:12:41 +0530

    new added

---
+++ Addition/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("Enter Value for A:");
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Value for B:");
+            b=Convert.ToInt32(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine("Addition is:"+ c);
+        }
+    }
+}
